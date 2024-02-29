6c50f27b92ce91be2a0861eaf1057d0076df788c
hashem <hashemhassen0@gmail.com>
2024-02-29 16:09:26 +0200

    setting up the app + packages

--- lib/features/home_view/presentation/view/home_view.dart
+++ lib/features/home_view/presentation/view/home_view.dart
@@ -5,8 +5,6 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(),
-    );
+    return Scaffold();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'features/home_view/presentation/view/home_view.dart';
 
-import 'features/home_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  const MaterialApp(
       title: 'Flutter Demo',
       home: HomeView(),
     );
