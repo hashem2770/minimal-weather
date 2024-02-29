6f8bb0f42da9c183c6e1ec93de77e258d0b16bcb
hashem <hashemhassen0@gmail.com>
2024-02-29 16:28:01 +0200

    create weather model

---
+++ lib/features/home_view/data/models/weather_model.dart
@@ -0,0 +1,25 @@
+class WeatherModel {
+  int? lat;
+  int? lon;
+  String? condition;
+  int? temp;
+  String? cityName;
+
+  WeatherModel({
+    required this.lat,
+    required this.lon,
+    required this.condition,
+    required this.temp,
+    required this.cityName,
+  });
+
+   factory WeatherModel.fromJson(Map<String, dynamic> json) {
+    return WeatherModel(
+      lat: json['coord']['lat'],
+      lon: json['coord']['lon'],
+      condition: json['weather'][0]['main'],
+      temp: json['main']['temp'],
+      cityName: json['name'],
+    );
+  }
+}
