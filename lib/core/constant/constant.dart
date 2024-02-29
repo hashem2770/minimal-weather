659174da69e5185995481692b4671ef007ab9f1d
hashem <hashemhassen0@gmail.com>
2024-02-29 16:56:51 +0200

    create repo impl

--- lib/core/constant/constant.dart
+++ lib/core/constant/constant.dart
@@ -1,2 +1,2 @@
 const String baseUrl='https://api.openweathermap.org/data/2.5/weather?';
-const String apiKey='2d833ae2f6c06bcb16755a2ebb6e0697'
+const String apiKey='2d833ae2f6c06bcb16755a2ebb6e0697';
--- lib/features/home_view/data/repos/weather_repo_impl.dart
+++ lib/features/home_view/data/repos/weather_repo_impl.dart
@@ -1,5 +1,41 @@
-class WeatherRepoImpl{
-  fetchWeatherByCoordinates(){
+import 'package:minimal_weather/features/home_view/data/models/weather_model.dart';
+import 'package:minimal_weather/features/home_view/data/web_services/web_services.dart';
+import 'package:geolocator/geolocator.dart';
 
+class WeatherRepoImpl{
+  WebService webService;
+
+  WeatherRepoImpl(this.webService);
+  Future<Position?> getLocation() async {
+    // Request location permission.
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    if (permission == LocationPermission.always || permission == LocationPermission.whileInUse) {
+      // Permission granted
+      try {
+        // Get the current location with high accuracy.
+        Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high,
+          timeLimit: const Duration(seconds: 10), // Set a timeout for requesting location.
+        );
+        return position;
+      } catch (e) {
+        // Handle other exceptions.
+        print("Error getting location: $e");
+        return null;
+      }
+    } else {
+      // Permission denied or permanently denied
+      print("Location permission denied");
+      return null;
+    }
+  }
+
+  Future<WeatherModel> fetchWeatherByCoordinates()async{
+    Position? position = await getLocation();
+    return webService.fetchWeatherByCoordinates(position!.latitude, position.longitude);
   }
 }
